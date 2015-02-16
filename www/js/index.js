d87a702f03b969cb6b427070520f5681d1c4bb9a
srinistoneham <srbale@gmail.com>
2015-02-16 00:41:35 -0500

    Display Location in Alert Box
    Display Location in Alert Box

--- www/js/index.js
+++ www/js/index.js
@@ -57,7 +57,8 @@ app.initialize();
     //
     function onSuccess(position) {
         var element = document.getElementById('geolocation');
-        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+        
+        var msg = 'Latitude: '           + position.coords.latitude              + '<br />' +
                             'Longitude: '          + position.coords.longitude             + '<br />' +
                             'Altitude: '           + position.coords.altitude              + '<br />' +
                             'Accuracy: '           + position.coords.accuracy              + '<br />' +
@@ -65,6 +66,9 @@ app.initialize();
                             'Heading: '            + position.coords.heading               + '<br />' +
                             'Speed: '              + position.coords.speed                 + '<br />' +
                             'Timestamp: '          + position.timestamp                    + '<br />';
+         element.innerHTML         
+         element.innerHTML  = msg;
+         alert(msg);
     }
 
     // onError Callback receives a PositionError object
